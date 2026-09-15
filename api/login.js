ce2774d284f3b9d35f3269ce7a11544b05df42ef
dwiiuwu01 <dwiiuwu01@gmail.com>
2026-09-16 03:00:50 +0700

    login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,14 @@
+export default function handler(req, res) {
+    const clientKey = process.env.TIKTOK_CLIENT_KEY;
+    const redirectUri = "https://post-connect-seven.vercel.app/callback";
 
+    const params = new URLSearchParams({
+        client_key: clientKey,
+        response_type: "code",
+        scope: "user.info.basic,video.publish",
+        redirect_uri: redirectUri,
+        state: "postconnect"
+    });
+
+    res.redirect(`https://www.tiktok.com/v2/auth/authorize/?${params.toString()}`);
+}
