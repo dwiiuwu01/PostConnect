07ca2f7d003b099a6af6fe22522dc338e402171f
dwiiuwu01 <dwiiuwu01@gmail.com>
2026-09-16 03:02:27 +0700

    callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,1 +1,13 @@
+export default function handler(req, res) {
+    const { code, state } = req.query;
 
+    if (!code) {
+        return res.status(400).send("Login TikTok gagal: authorization code tidak ditemukan.");
+    }
+
+    res.send(`
+        <h1>Login TikTok Berhasil!</h1>
+        <p>Authorization code berhasil diterima.</p>
+        <p>Sekarang PostConnect sudah menerima respons dari TikTok.</p>
+    `);
+}
